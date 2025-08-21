786ab38d516e548a1349e4cef2a52689d56336c3
AguArias11 <aariascovnatsky@gmail.com>
2025-08-21 17:34:04 +0000

    Terminado

--- client/src/client.c
+++ client/src/client.c
@@ -42,11 +42,15 @@ int main(void)
 	// ADVERTENCIA: Antes de continuar, tenemos que asegurarnos que el servidor esté corriendo para poder conectarnos a él
 
 	// Creamos una conexión hacia el servidor
+
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
 
+	enviar_mensaje(valor,conexion);
+
 	// Armamos y enviamos el paquete
+	
 	paquete(conexion);
 
 	terminar_programa(conexion, logger, config);
@@ -92,7 +96,7 @@ void leer_consola(t_log* logger)
 	while(strcmp(leido,"")!=0){
 		free(leido);
 		leido = readline("> ");
-		log_info(logger,"%s",leido);
+		log_info(logger, "%s", leido);
 		// Loggeamos el valor de config
 	}
 	
@@ -104,13 +108,25 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
 
+	leido = readline("> ");
+	agregar_a_paquete(paquete, leido, strlen(leido) +1);
+	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+
+	while(strcmp(leido,"")!=0){
+		free(leido);
+		leido = readline("> ");
+		agregar_a_paquete(paquete, leido, strlen(leido) +1);
+	}
+	free(leido);
+
+	enviar_paquete(paquete,conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -119,4 +135,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	log_destroy(logger);
 	config_destroy(config);
+	liberar_conexion(conexion);
+
 }
